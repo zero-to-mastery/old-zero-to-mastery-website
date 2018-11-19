6afc40a1972c5d9c57c06a74e613a82752a78cd0
MattCSmith <mrmcchow@gmail.com>
2018-11-19 17:07:45 +0000

    Advent layout

--- assets/js/advent.js
+++ assets/js/advent.js
@@ -1,30 +1,32 @@
+const url = window.location.href
+const paramIndex = url.indexOf("?")
+const fetchLink = (paramIndex === -1 ? "https://cors.io/?http://91.121.210.171:42550/solutions/all" : `https://cors.io/?http://91.121.210.171:42550/solutions/${url.slice(paramIndex)}`)
 
-let url = window.location.href
-let paramLocation = url.indexOf("1")
-let selection = url.slice(url.indexOf("?"))
-console.log("selection", selection)
-let fetchLink = (selection === "l" ? "https://cors.io/?http://91.121.210.171:42550/solutions/all" : `https://cors.io/?http://91.121.210.171:42550/solutions/${selection}`)
 
-const dataFetch1 = () => {
+
+const fetchSolutions = () => {
     fetch(fetchLink).then(response => {
         return response.json();
     }).then(data => {
         data.forEach(sol => {
-            console.log(sol)
+
+
             let dayImgUrl = "../assets/images/days/" + sol.dayNumber + ".png";
-            console.log(dayImgUrl)
+            const tooltip = "Day-" + sol.dayNumber
+
             document.getElementById('solutions').insertAdjacentHTML('beforeend', 
             `
-            <div class="card coursecard">
-            <img src="https://cdn2.iconfinder.com/data/icons/nodejs-1/512/nodejs-512.png" class="langIcon">
-            <img src=${dayImgUrl} class="dayIcon">
-            <img class="card-img-top" src=${sol.avatarUrl}
-                alt="Card image cap">
-            <div class="card-body text-center">
-                <h5 class="card-title">${sol.userName}</h5>
-                <a href=${sol.url} class="btn btn-outline-primary mt-3 mb-0">View Solution</a>
+            <div class="my-3 col-sm-6 col-md-4 col-lg-3">
+                <div class="card adventCard">
+                    <img src="https://cdn2.iconfinder.com/data/icons/nodejs-1/512/nodejs-512.png" class="langIcon">
+                    <img src=${dayImgUrl} class="dayIcon" data-toggle="tooltip" data-placement="top" title=${tooltip}>
+                    <img class="card-img-top img-fluid" src=${sol.avatarUrl} alt=${sol.userName}>
+                    <div class="card-body text-center bg-dark">
+                        <h5 class="card-title">${sol.userName}</h5>
+                        <a href=${sol.url} class="btn btn-outline-warning btn-sm mt-3 mb-0">View Solution</a>
+                    </div>
+                </div>
             </div>
-        </div>
             `);
 
         });
@@ -33,8 +35,10 @@ const dataFetch1 = () => {
     });
 }
 
-dataFetch1();
+fetchSolutions();
 
+
+// Modal Button Generator
 let date = new Date();
 let day = date.getDate()
 console.log(day)
