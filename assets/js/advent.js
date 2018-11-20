4b32c3ac0aebd33efde51f82e417aec2144ee47a
Ankur Anant <ankuranant99@gmail.com>
2018-11-20 15:46:38 +0530

    added clear-filter, fixed todays sub

--- assets/js/advent.js
+++ assets/js/advent.js
@@ -122,11 +122,16 @@ while (day > 0) {
 
 playerModal = (u) => {
     console.log(u)
+    document.getElementById("userModal-points").innerHTML = 
+    `<i class="fa fa-spinner fa-spin" style="color: #D4AF37; font-size: 0.8em"></i>`;
     fetch("https://cors.io/?http://91.121.210.171:42550/user").then(response => {
         return response.json();
     }).then(data => {
         let obj = data.find(obj => obj.username === u);
-        document.getElementById("userContent").innerHTML = `<h5 class="p-5 m-5">Points: ${obj.point}</h5>`;
+        document.getElementById("userModal-points").innerHTML = "Points: "+obj.point;
+        // document.getElementById("userContent").innerHTML = 
+        // `<h5 class="p-5">Name: ${obj.username}</h5>
+        // <h5 class="p-5">Points: ${obj.point}</h5>`;
         console.log("User Obj", obj)
     }).catch(err => {
         throw err;
