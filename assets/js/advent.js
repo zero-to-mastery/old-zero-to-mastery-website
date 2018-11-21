4ab0b3e08486a912daab0ba88d901f2a10334694
MattCSmith <mrmcchow@gmail.com>
2018-11-21 12:39:59 +0000

    Dynamic Title

--- assets/js/advent.js
+++ assets/js/advent.js
@@ -12,15 +12,15 @@ const stats = async () => {
 }
 stats()
 
-// Fetch Users
+// Fetch Users for Dropdown
 const users = async () => {
     fetch("https://cors.io/?http://91.121.210.171:42550/user").then(response => {
         return response.json();
     }).then(data => {
         data.forEach(user => {
             let uLink = "?user=" + user.username
-            document.getElementById("studentFilter").insertAdjacentHTML('beforeend',  
-            `<a class="dropdown-item" id=${uLink} onclick=onClick(this.id)>${user.username}</a>`)
+            document.getElementById("studentFilter").insertAdjacentHTML('beforeend',
+                `<a class="dropdown-item" id=${uLink} onclick=onClick(this.id)>${user.username}</a>`)
         });
     }).catch(err => {
         throw err;
@@ -30,14 +30,12 @@ users()
 
 const onClick = (param) => {
     document.getElementById("solutions").innerHTML = `<h3 class="my-5 w-100">Loading...</h3>`;
-    if(param === "clear") fetchSolutions("solutions/all");
-    if(param.includes("user")) displayUser(param);
-    if(param.includes("day")) {
+    if (param === "clear") fetchSolutions("solutions/all");
+    if (param.includes("user")) displayUser(param);
+    if (param.includes("day")) {
         $('#exampleModal').modal('hide');
-        fetchSolutions("solutions/"+param)
+        fetchSolutions("solutions/" + param)
     }
-
-    console.log("Clicked", param)
 }
 
 // Fetch Solution
@@ -45,15 +43,20 @@ const fetchSolutions = (url) => {
     fetch("https://cors.io/?http://91.121.210.171:42550/" + url).then(response => {
         return response.json();
     }).then(data => {
+        console.log(url)
         document.getElementById("solutions").innerHTML = ``;
-       if(data.length < 1) {
-        document.getElementById("solutions").innerHTML = `<h3 class="my-5 w-100">Nothing to display</h3>`;
-       }
-       
+        if (data.length < 1) {
+            document.getElementById("solutions").innerHTML = `<h3 class="my-5 w-100">Nothing to display</h3>`;
+        }
+        if(url.includes("solutions/all")) document.getElementById("adventTitle").innerHTML = `All Student's Solutions`;
+        if(url.includes("solutions/?day")) { 
+            
+            document.getElementById("adventTitle").innerHTML = `Solutions for ${url.slice(15)} December`;
+        }
         data.forEach(sol => {
             insertCard(sol)
         });
-        
+
     }).catch(err => {
         throw err;
     });
@@ -67,16 +70,16 @@ const displayUser = (user) => {
         return response.json();
     }).then(data => {
         document.getElementById("solutions").innerHTML = ``;
-       if(data.length < 1) {
-        document.getElementById("solutions").innerHTML = `<h3 class="my-5 w-100">Nothing to display</h3>`;
-       }
-       
+        if (data.length < 1) {
+            document.getElementById("solutions").innerHTML = `<h3 class="my-5 w-100">Nothing to display</h3>`;
+        }
+        document.getElementById("adventTitle").innerHTML = `${name}'s Solutions`;
         data.forEach(sol => {
-            if(sol.userName === name){
-            insertCard(sol)
+            if (sol.userName === name) {
+                insertCard(sol)
             }
         });
-        
+
     }).catch(err => {
         throw err;
     });
@@ -85,21 +88,21 @@ const displayUser = (user) => {
 
 const qIndex = window.location.href.indexOf("?")
 let qParam = (qIndex === -1 ? "solutions/all" : window.location.href.slice(qIndex))
-if(qParam.includes("?day=")) fetchSolutions("solutions/" + qParam)
-if(qParam.includes("?user=")) displayUser(qParam)
-if(qParam === "solutions/all") fetchSolutions(qParam)
+if (qParam.includes("?day=")) fetchSolutions("solutions/" + qParam)
+if (qParam.includes("?user=")) displayUser(qParam)
+if (qParam === "solutions/all") fetchSolutions(qParam)
 console.log(qParam)
 
 
 const insertCard = (sol) => {
     let dayImgUrl = "../assets/images/days/" + sol.dayNumber + ".png";
     const tooltip = "Day-" + sol.dayNumber
-    console.log("111",Object.keys(sol).length)
-    if(Object.keys(sol).length === 0){
+    console.log("111", Object.keys(sol).length)
+    if (Object.keys(sol).length === 0) {
         console.log("NOPE")
     }
-    document.getElementById('solutions').insertAdjacentHTML('beforeend', 
-    `
+    document.getElementById('solutions').insertAdjacentHTML('beforeend',
+        `
         <div class="my-3 col-sm-6 col-md-4 col-lg-3">
             <div class="card adventCard">
                 <img src="https://cdn2.iconfinder.com/data/icons/nodejs-1/512/nodejs-512.png" class="langIcon">
@@ -127,24 +130,24 @@ let day = date.getDate()
 while (day > 0) {
     let dayBtn = "../assets/images/days/" + day + ".png";
     let link = `?day=` + day
-    document.getElementById('calBtn').insertAdjacentHTML('beforeend', 
-    `
+    document.getElementById('calBtn').insertAdjacentHTML('beforeend',
+        `
     <a  id=${link} onclick=onClick(this.id)><img src=${dayBtn}></a>
     
 
     `);
-  day --
+    day--
 }
 
 playerModal = (u) => {
     console.log(u)
-    document.getElementById("userModal-points").innerHTML = 
-    `<i class="fa fa-spinner fa-spin" style="color: #D4AF37; font-size: 0.8em"></i>`;
+    document.getElementById("userModal-points").innerHTML =
+        `<i class="fa fa-spinner fa-spin" style="color: #D4AF37; font-size: 0.8em"></i>`;
     fetch("https://cors.io/?http://91.121.210.171:42550/user").then(response => {
         return response.json();
     }).then(data => {
         let obj = data.find(obj => obj.username === u);
-        document.getElementById("userModal-points").innerHTML = "Points: "+obj.point;
+        document.getElementById("userModal-points").innerHTML = "Points: " + obj.point;
         // document.getElementById("userContent").innerHTML = 
         // `<h5 class="p-5">Name: ${obj.username}</h5>
         // <h5 class="p-5">Points: ${obj.point}</h5>`;
