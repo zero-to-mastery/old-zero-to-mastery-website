92bd59496d5bed824c89f4e157fd75af659a70bd
lupuselit <lupuselit@outlook.com>
2019-01-01 16:14:51 -0200

    change year on the footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,3 +2,11 @@ function play(video){
     document.getElementById('vidwrap').innerHTML = '<iframe width="560" height="315" src="' + video + '"frameborder="0"></iframe>';
     document.getElementById("vidwrap").classList.remove("playicon");
   };
+
+  // update the year dinamically with a immediately invoked function expression
+(function () {
+  // gets the span wrapped around the year
+  const year = document.querySelector('.year-footer');
+  // updates the year with the current year
+  year.innerHTML = new Date().getFullYear();
+})();
