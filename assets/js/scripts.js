787a06a3960329e183f96c7b5faecd7da3fe803a
HitzeD <d-hitze@comcast.net>
2019-04-10 18:40:06 -0500

    added dynamic year to all dates throughout webpage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,6 +7,15 @@ function play(video){
 (function () {
   // gets the span wrapped around the year
   const year = document.querySelector('.year-footer');
+  const yearSaver = document.querySelector('.year');
   // updates the year with the current year
   year.innerHTML = new Date().getFullYear();
 })();
+
+function getDate(){
+  const yearSaver = document.querySelectorAll('.year');
+  yearSaver.forEach(value => value.innerHTML = new Date().getFullYear());
+}
+
+setInterval(getDate, 5000);
+getDate();
