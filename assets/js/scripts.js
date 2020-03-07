b92d0a43428ffb3b83d0df71f627fe863a0c21f0
Allan Oliveira <allanrj@gmail.com>
2020-03-07 22:58:50 +0000

    Added Machine Learning and Data Science Course and consolidate the Navigation and Footer in specific files to avoid repetition

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,19 +3,15 @@ function play(video){
     document.getElementById("vidwrap").classList.remove("playicon");
   };
 
-  // update the year dinamically with a immediately invoked function expression
-(function () {
-  // gets the span wrapped around the year
-  const year = document.querySelector('.year-footer');
-  // updates the year with the current year
-  year.innerHTML = new Date().getFullYear();
-})();
-
 function getDate(){
   // grabbing all items with the class of year
   const yearSaver = document.querySelectorAll('.year');
   // looping through the nodelist and adding the year to each span element
   yearSaver.forEach(value => value.innerHTML = new Date().getFullYear());
 }
-
 getDate();
+
+function getDateFooter(){
+  const year = document.querySelector('.year-footer');
+  year.innerHTML = new Date().getFullYear();
+}
